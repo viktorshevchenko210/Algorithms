88e026f4c238ddcdfc92a23b3584a26289db364f
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-02-03 21:55:43 +0200

    ValidParentheses_20

--- LeetCode/Easy/ValidParentheses_20/ValidParentheses_20/Program.cs
+++ LeetCode/Easy/ValidParentheses_20/ValidParentheses_20/Program.cs
@@ -10,6 +10,76 @@ namespace ValidParentheses_20
     {
         static void Main(string[] args)
         {
+            string input = "([]";
+            bool isvalid = IsValid(input);
+            Console.WriteLine(isvalid);
+            Console.ReadKey();
+        }
+
+        public static bool IsValid(string s)
+        {
+            if (s.Length <= 1)
+                return false;
+
+            var result = false;
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char current = s[i];
+                if (IsOpen(current))
+                {
+                    stack.Push(current);
+                    result = false;
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                        return false;
+
+                    if (GetOpposite(stack.Pop()) != current)
+                        return false;
+
+                    result = true;
+                }
+            }
+
+            if (stack.Count != 0)
+                return false;
+
+            return result;
+        }
+
+        private static bool IsOpen(char a)
+        {
+            if (a == '(' || a == '[' || a == '{')
+                return true;
+
+            return false;
+        }
+
+        private static char GetOpposite(char s)
+        {
+            switch (s)
+            {
+                case '(':
+                    return ')';
+                case '[':  
+                    return ']';
+                case '{':  
+                    return '}';
+
+
+                case ')':  
+                    return '(';
+                case ']':  
+                    return '[';
+                case '}':  
+                    return '{';
+            }
+
+            return '\0';
         }
     }
 }
