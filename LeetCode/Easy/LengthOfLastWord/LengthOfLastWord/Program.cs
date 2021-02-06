3e1b5eddd0a102003195f8f26a722c42ecbacaff
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-02-06 02:13:22 +0200

    no message

---
+++ LeetCode/Easy/LengthOfLastWord/LengthOfLastWord/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthOfLastWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "day     ";
+            var length = LengthOfLastWord(input);
+            Console.WriteLine(length);
+            Console.ReadLine();
+        }
+
+        public static int LengthOfLastWord(string s)
+        {
+            int length = 0;
+
+            int startIndex = 0;
+            int endIndex = s.Length - 1;
+
+            while (startIndex <= endIndex)
+            {
+                if (s[endIndex] == ' ')
+                {
+                    endIndex--;
+                    continue;
+                }
+
+                if (s[startIndex] != ' ')
+                {
+                    startIndex++;
+                    length++;
+                }
+                else
+                {
+                    length = 0;
+                    startIndex++;
+                }
+
+            }
+
+            return length;
+        }
+    }
+}
