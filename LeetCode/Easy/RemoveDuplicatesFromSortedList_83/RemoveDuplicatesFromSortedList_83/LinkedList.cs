3fbe977df4efaf8e7c7b773fd3585c959114cf49
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-02-07 20:34:52 +0200

    no message

---
+++ LeetCode/Easy/RemoveDuplicatesFromSortedList_83/RemoveDuplicatesFromSortedList_83/LinkedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoveDuplicatesFromSortedList_83
+{
+  public class ListNode
+   {
+      public int val;
+      public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+       {
+           this.val = val;
+           this.next = next;
+       }
+  }
+}
---
+++ LeetCode/Easy/RemoveDuplicatesFromSortedList_83/RemoveDuplicatesFromSortedList_83/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RemoveDuplicatesFromSortedList_83
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(1);
+            head.next.next = new ListNode(1);
+            head.next.next = new ListNode(2);
+            head.next.next.next = new ListNode(3);
+            head.next.next.next.next = new ListNode(3);
+
+
+            var listNode = DeleteDuplicates(head);
+
+        }
+
+        static ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null)
+                return head;
+
+            var current = head;
+            while(current != null && current.next != null)
+            {
+                if(current.next.val == current.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
