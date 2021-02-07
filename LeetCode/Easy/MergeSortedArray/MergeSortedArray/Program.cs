41e14c873522dfef0983e9d617d12f7f9772294e
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-02-08 00:57:58 +0200

    no message

--- LeetCode/Easy/MergeSortedArray/MergeSortedArray/Program.cs
+++ LeetCode/Easy/MergeSortedArray/MergeSortedArray/Program.cs
@@ -6,7 +6,102 @@ namespace MergeSortedArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+            //int m = 3;
+
+            //int[] nums2 = new int[] { 2, 5, 6 };
+            //int n = 3;
+
+            int[] nums1 = new int[] { 4,5,6, 0,0,0 };
+            int m = 3;
+
+            int[] nums2 = new int[] { 1,2,3 };
+            int n = 3;
+
+            OptimalMerge(nums1, m, nums2, n);
+        }
+
+        static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+
+            int nums2Index = 0;
+            int insertedIndex = m;
+
+            int lastInsertedIndex = m;
+
+            int totalLength = m + n;
+
+            while (insertedIndex != totalLength)
+            {
+                if(insertedIndex == 0)
+                {
+                    nums1[insertedIndex] = nums2[nums2Index];
+                    insertedIndex++;
+                    nums2Index++;
+                    lastInsertedIndex++;
+                    continue;
+                }
+
+                if (nums1[lastInsertedIndex - 1] > nums2[nums2Index])
+                {
+                    nums1[insertedIndex] = nums1[lastInsertedIndex - 1];
+                    nums1[lastInsertedIndex - 1] = nums2[nums2Index];
+
+                    insertedIndex++;
+                    lastInsertedIndex--;
+
+                    while (lastInsertedIndex != 0 && nums1[lastInsertedIndex] < nums1[lastInsertedIndex-1])
+                    {
+                        var temp = nums1[lastInsertedIndex];
+                        nums1[lastInsertedIndex] = nums1[lastInsertedIndex - 1];
+                        nums1[lastInsertedIndex - 1] = temp;
+
+                        lastInsertedIndex--;
+
+                    }
+
+                    lastInsertedIndex = insertedIndex;
+                    nums2Index++;
+                }
+                else
+                {
+                    nums1[insertedIndex] = nums2[nums2Index];
+                    insertedIndex++;
+                    nums2Index++;
+                }
+            }
+        }
+
+        static void OptimalMerge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int p = m + n - 1;
+
+            m = m - 1;
+            n = n - 1;
+
+            while(m >= 0 && n >= 0)
+            {
+                if(nums1[m] >= nums2[n])
+                {
+                    nums1[p] = nums1[m];
+                    m--;
+                }
+                else
+                {
+                    nums1[p] = nums2[p];
+                    n--;
+                }
+
+                p--;
+            }
+
+            while(n >= 0)
+            {
+
+                nums1[p] = nums2[n];
+                n--;
+                p--;
+            }
         }
     }
 }
---
+++ LeetCode/Easy/SameTree_100/SameTree_100/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SameTree_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode firstTree = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+            TreeNode secondTree = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+
+            var isSame = IsSameTree(firstTree, secondTree);
+            Console.WriteLine(isSame);
+
+            Console.ReadKey();
+        }
+
+        static bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+
+            if (p == null && q != null)
+                return false;
+
+            if (q == null && p != null)
+                return false;
+
+            if (p.val != q.val)
+                return false;
+
+            var isSame = IsSameTree(p.left, q.left);
+            if (!isSame)
+                return false;
+
+            isSame = IsSameTree(p.right, q.right);
+            if (!isSame)
+                return false;
+
+            return true;
+        }
+
+
+    }
+}
