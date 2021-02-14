322391cb3a32136c7346facf3898d9d21a0c1c69
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-02-15 01:54:10 +0200

    no message

---
+++ LeetCode/Easy/MinStack_155/MinStack_155/MinStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinStack_155
+{
+    public class MinStack
+    {
+        List<int> list;
+        int count;
+
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+            count = 0;
+            list = new List<int>();
+        }
+
+        public void Push(int x)
+        {
+            if (list.Count == count)
+                list.Add(x);
+            else
+            {
+                list[count] = x;
+                //if(count == 0)
+                //    list[count] = x;
+                //else
+                //{
+                //    list[count - 1] = x;
+                //}
+            }
+            count++;
+        }
+
+        public void Pop()
+        {
+            list[--count] = 0;
+        }
+
+        public int Top()
+        {
+            return list[count-1];
+        }
+
+        public int GetMin()
+        {
+            return list.GetRange(0, count).Min();
+        }
+    }
+}
---
+++ LeetCode/Easy/MinStack_155/MinStack_155/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MinStack_155
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinStack minStack = new MinStack();
+
+            minStack.Push(2147483646);
+            minStack.Push(2147483646);
+            minStack.Push(2147483647);
+
+            var top1 = minStack.Top();
+            minStack.Pop();
+
+            var min1 = minStack.GetMin();
+            minStack.Pop();
+
+            var min2 = minStack.GetMin();
+            minStack.Pop();
+
+            minStack.Push(2147483647);
+
+            var top2 = minStack.Top();
+
+            var min3 = minStack.GetMin();
+
+            minStack.Push(-2147483648);
+
+            var top3 = minStack.Top();
+
+            var min4 = minStack.GetMin();
+
+            minStack.Pop();
+
+            var min5 = minStack.GetMin();
+        }
+    }
+}
