d9e9b71f29b983f57e62c63038fe95eba3d455b2
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-02-17 01:45:28 +0200

    no message

---
+++ LeetCode/Easy/FactorialTrailingZeroes_172/FactorialTrailingZeroes_172/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+
+namespace FactorialTrailingZeroes_172
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrailingZeroes(13);
+        }
+
+        static int TrailingZeroes(int n)
+        {
+            long result = 1;
+
+            for(int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            int zeroesCount = 0;
+
+
+            while(result != 0)
+            {
+                if(result % 10 != 0)
+                {
+                    return zeroesCount;
+                }
+                else
+                {
+                    zeroesCount++;
+                }
+
+                result = result / 10;
+            }
+            
+
+            return zeroesCount;
+        }
+    }
+}
---
+++ LeetCode/Easy/MajorityElement_169/MajorityElement_169/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MajorityElement_169
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3, 2, 3 };
+            var result = MajorityElement(nums);
+        }
+
+        static int MajorityElement(int[] nums)
+        {
+            int count = 0;
+            int candidate = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(count == 0)
+                {
+                    candidate = nums[i];
+                }
+
+                if(candidate == nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+
+            return candidate;
+        }
+    }
+}
