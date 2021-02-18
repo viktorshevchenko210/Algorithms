076be9d578c013a6f9c55dc055b8592da0e8cfc5
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-02-19 00:12:36 +0200

    no message

---
+++ LeetCode/Easy/PlusOne_66/PlusOne_66/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PlusOne_66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits = new int[] { 9 };
+            PlusOne(digits);
+        }
+
+        static int[] PlusOne(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; --i)
+            {
+                if (digits[i] == 9)
+                {
+                    digits[i] = 0;
+                }
+                else
+                {
+                    digits[i] = digits[i] + 1;
+                    return digits;
+                }
+            }
+
+            int[] result = new int[digits.Length + 1];
+            result[0] = 1;
+            Array.Copy(digits, 0, result, 1, digits.Length);
+            return result;
+
+            //var lastNumber = digits[digits.Length - 1] + 1;
+            //var numbersCount = (int)Math.Ceiling(Math.Log10(Math.Abs(lastNumber) + 0.5));
+
+            //if(numbersCount == 1)
+            //{
+            //    digits[digits.Length - 1] = digits[digits.Length - 1] + 1;
+            //    return digits;
+            //}
+
+            //int[] result = new int[digits.Length+numbersCount-1];
+
+            //for(int i = 0; i < digits.Length - 1; i++) 
+            //{
+            //    result[i] = digits[i];
+            //}
+
+            //for (int i = result.Length-1; i >= digits.Length-1; i--)
+            //{
+            //    var j = lastNumber % 10;
+            //    lastNumber = lastNumber / 10;
+            //    result[i] = j;
+            //}
+
+            //return result;
+        }
+    }
+}
---
+++ LeetCode/Easy/SymmetricTree/SymmetricTree/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace SymmetricTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode tree = new TreeNode(2);
+            tree.left = new TreeNode(3);
+            tree.left.left = new TreeNode(4);
+            tree.left.right = new TreeNode(5);
+
+            tree.right = new TreeNode(3);
+            tree.right.right = new TreeNode(4);
+
+            //TreeNode tree = new TreeNode(1);
+            //tree.left = new TreeNode(2);
+            //tree.left.left = new TreeNode(3);
+            //tree.left.right = new TreeNode(4);
+
+            //tree.right = new TreeNode(2);
+            //tree.right.right = new TreeNode(3);
+            //tree.right.left = new TreeNode(4);
+
+            IsSymmetric(tree);
+        }
+
+        static bool IsSymmetric(TreeNode root)
+        {
+            return IsSymmetricRecursive(root.left, root.right);
+        }
+
+        static bool IsSymmetricRecursive(TreeNode left, TreeNode right)
+        {
+            if (left == null && right != null)
+                return false;
+
+            if (right == null && left != null)
+                return false;
+
+            if (left == null && right == null)
+                return true;
+
+            if (right.val != left.val)
+                return false;
+
+            if (!IsSymmetricRecursive(left.left, right.right))
+                return false;
+
+            if (!IsSymmetricRecursive(left.right, right.left))
+                return false;
+
+            return true;
+        }
+
+        //static bool IsSymmetricRecursive(TreeNode left, TreeNode right, ref bool result)
+        //{
+        //    if (left == null && right != null)
+        //        return false;
+
+        //    if (right == null && left != null)
+        //        return false;
+
+        //    if (left == null && right == null)
+        //        return true;
+
+        //    if (right.val != left.val)
+        //        return false;
+
+        //    if (left.left != null)
+        //        return IsSymmetricRecursive(left.left, right.right);
+
+
+        //        return  IsSymmetricRecursive(left.right, right.left);
+        //}
+    }
+}
