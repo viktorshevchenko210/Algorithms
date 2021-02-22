f2404c9dc7df060eb9ad1202fcb5617201464a1d
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-02-22 16:08:06 +0200

    no message

---
+++ LeetCode/Easy/HappyNumber_202/HappyNumber_202/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HappyNumber_202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            var isHappy = IsHappy(n);
+
+            Console.WriteLine(isHappy);
+            Console.ReadKey();
+        }
+
+        static bool IsHappy(int n)
+        {
+            HashSet<int> numbers = new HashSet<int>();
+
+            while (true)
+            {
+                var nNew = 0;
+
+                while(n > 0)
+                {
+                    var i = n % 10;
+                    nNew += i * i;
+
+                    n = n / 10;
+                }
+
+                if (nNew == 1)
+                    return true;
+
+                n = nNew;
+
+                if (numbers.Contains(n))
+                    return false;
+                else
+                    numbers.Add(n);
+            }
+        }
+    }
+}
