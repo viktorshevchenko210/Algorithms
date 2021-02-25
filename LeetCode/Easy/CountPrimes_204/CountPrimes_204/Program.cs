ae3191ad5937094811e6fa46b5b321351baa8101
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-02-26 01:25:55 +0200

    no message

---
+++ LeetCode/Easy/CountPrimes_204/CountPrimes_204/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountPrimes_204
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 499979;
+            CountPrimes(n);
+        }
+
+        static int CountPrimes(int n)
+        {
+            HashSet<int> excluded = new HashSet<int>();
+
+            int count = 0;
+
+            int i = 2;
+
+            while (i <= n)
+            {
+                if (!excluded.Contains(i))
+                {
+                    int index = 0;
+
+                    int cur = i * i;
+
+                    for (int j = cur; j <= n; j = cur + index * i)
+                    {
+                        excluded.Add(j);
+                        index++;
+                    }
+
+                    count++;
+                }
+
+                i++;
+            }
+            return count;
+        }
+    }
+}
