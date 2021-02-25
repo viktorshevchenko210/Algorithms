62442691c48e99428462f8b117e16abb3e426cf9
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-02-26 00:03:36 +0200

    no message

---
+++ LeetCode/Easy/RemoveLinkedListElements_203/RemoveLinkedListElements_203/ListNode.cs
@@ -0,0 +1,14 @@
+﻿
+namespace RemoveLinkedListElements_203
+{
+  public class ListNode
+  {
+      public int val;
+      public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+      {
+           this.val = val;
+           this.next = next;
+      }
+  }
+}
---
+++ LeetCode/Easy/RemoveLinkedListElements_203/RemoveLinkedListElements_203/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace RemoveLinkedListElements_203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            //head.next = new ListNode(2);
+            //head.next.next = new ListNode(6);
+            //head.next.next.next = new ListNode(3);
+            //head.next.next.next.next = new ListNode(4);
+            //head.next.next.next.next.next = new ListNode(5);
+            //head.next.next.next.next.next.next = new ListNode(6);
+
+            int val = 1;
+
+            RemoveElements(head, val);
+        }
+
+        static ListNode RemoveElements(ListNode head, int val)
+        {
+
+            ListNode previous = null;
+            var headNext = head;
+
+            while (headNext != null)
+            {
+                if (headNext.val == val)
+                {
+                    if (previous == null)
+                    {
+                        headNext = headNext.next;
+                        head = head.next;
+                        continue;
+                    }
+                    else
+                    {
+                        previous.next = headNext.next;
+                        headNext = previous;
+                        continue;
+                    }
+                }
+
+                previous = headNext;
+                headNext = headNext.next;
+            }
+
+            return head;
+        }
+    }
+}
