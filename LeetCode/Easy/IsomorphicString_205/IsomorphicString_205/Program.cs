fd82cf91a836bd8e0d78366a6deb7e634ef99f29
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-03-02 01:03:08 +0200

    no message

---
+++ LeetCode/Easy/IsomorphicString_205/IsomorphicString_205/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsomorphicString_205
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "egg";
+            string t = "add";
+            IsIsomorphic(s,t);
+        }
+
+        static bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            Dictionary<char, char> mapS = new Dictionary<char, char>();
+            Dictionary<char, char> mapT = new Dictionary<char, char>();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                var sI = s[i];
+                var tI = t[i];
+
+                if(mapS.TryGetValue(sI, out char val))
+                {
+                    if (tI != val)
+                        return false;
+                }
+                else
+                {
+                    mapS.Add(sI, tI);
+                }
+
+                if (mapT.TryGetValue(tI, out char val2))
+                {
+                    if (sI != val2)
+                        return false;
+                }
+                else
+                {
+                    mapT.Add(tI, sI);
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/Easy/ReverseLinkedList/ReverseLinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ReverseLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(5);
+        }
+
+        static ListNode ReverseList(ListNode head)
+        {
+            ListNode newHead = null;
+
+            while (head != null)
+            {
+                if (newHead == null)
+                    newHead = new ListNode(head.val);
+                else
+                {
+                    var node = new ListNode(head.val);
+                    node.next = newHead;
+                    newHead = node;
+                }
+
+                head = head.next;
+            }
+
+            return newHead;
+        }
+    }
+}
