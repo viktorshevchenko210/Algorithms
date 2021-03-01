864b8bd19ebb3ae10e02b61c244a7546f61f5020
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-03-02 01:13:53 +0200

    no message

---
+++ LeetCode/Easy/ContainsDuplicate_219/ContainsDuplicate_219/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ContainsDuplicate_219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 1 };
+            int k = 3;
+            ContainsNearbyDuplicate(array, k);
+        }
+
+        static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                var current = nums[i];
+                var next = nums[i + 1];
+
+                if (Math.Abs(current - next) >= 3)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
