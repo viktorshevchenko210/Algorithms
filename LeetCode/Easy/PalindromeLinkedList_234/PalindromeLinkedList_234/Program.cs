9be5b49ecedd7d9fe10612384190e60b9036e99c
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-03-02 16:09:45 +0200

    no message

---
+++ LeetCode/Easy/PalindromeLinkedList_234/PalindromeLinkedList_234/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PalindromeLinkedList_234
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(2);
+            head.next.next.next = new ListNode(2);
+            head.next.next.next.next = new ListNode(1);
+            IsPalindrome(head);
+        }
+
+        static bool IsPalindrome(ListNode head)
+        {
+            bool isOdd = false;
+
+            int size = GetLength(head);
+            if (size == 1)
+                return true;
+
+            if (size % 2 != 0)
+                isOdd = true;
+
+            int half = size / 2;
+
+            var current = head;
+            int step = 0;
+
+            ListNode reversedLeftHalf = null;
+
+            while (current != null)
+            {
+                if (isOdd)
+                {
+                 
+                    if(step > half)
+                    {
+                        if (current.val != reversedLeftHalf.val)
+                            return false;
+
+                        reversedLeftHalf = reversedLeftHalf.next;
+                    }
+                }
+                else
+                {
+                    if(step >= half)
+                    {
+                        if (current.val != reversedLeftHalf.val)
+                            return false;
+
+                        reversedLeftHalf = reversedLeftHalf.next;
+                    }
+                }
+
+                if (step < half)
+                {
+                    if (reversedLeftHalf == null)
+                        reversedLeftHalf = new ListNode(current.val);
+                    else
+                    {
+                        var node = new ListNode(current.val);
+                        node.next = reversedLeftHalf;
+                        reversedLeftHalf = node;
+                    }
+                }
+
+                step++;
+                current = current.next;
+            }
+
+            return true;
+        }
+
+
+        static int GetLength(ListNode head)
+        {
+            int size = 0;
+
+            var current = head;
+            while(head != null)
+            {
+                size++;
+                head = head.next;
+            }
+
+            return size;
+        }
+    }
+}
---
+++ LeetCode/Easy/SummaryRanges_228/SummaryRanges_228/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummaryRanges_228
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 0, 1, 2, 4, 5, 7 };
+            SummaryRanges(array);
+        }
+
+        static IList<string> SummaryRanges(int[] nums)
+        {
+            List<string> ranges = new List<string>();
+
+            bool isRange = false;
+
+            int steps = 0;
+            int startRange = 0;
+
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                var current = nums[i];
+                var next = nums[i + 1];
+
+                if(next - current == 1)
+                {
+                    if (isRange)
+                    {
+                        steps++;
+                        continue;
+                    }
+                    startRange = current;
+                    isRange = true;
+                }
+                else
+                {
+                    if (isRange)
+                    {
+                        if(steps > 0)
+                        {
+                            ranges.Add($"{startRange}->{current}");
+                        }
+                        else
+                        {
+                            ranges.Add($"{startRange}");
+                        }
+                        isRange = false;
+                    }
+                    else
+                    {
+                        ranges.Add($"{current}");
+                    }
+                }
+            }
+
+            return ranges;
+        }
+    }
+}
