ef2c7ce0e7eb1974b47347994e16f42d39c6e914
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-03-04 01:24:44 +0200

    no message

---
+++ LeetCode/Easy/AddDigits_258/AddDigits_258/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AddDigits_258
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            AddDigits(num);
+        }
+
+        static int AddDigits(int num)
+        {
+            while ((int)Math.Ceiling(Math.Log10(Math.Abs(num) + 0.5)) != 1 && num != 0)
+            {
+                var oldNum = num;
+                var result = 0;
+                while (oldNum != 0)
+                {
+                    var lastNumber = oldNum % 10;
+                    result += lastNumber;
+                    oldNum /= 10;
+                }
+                num = result;
+            }
+
+            return num;
+        }
+    }
+}
---
+++ LeetCode/Easy/BinaryTreePaths_257/BinaryTreePaths_257/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTreePaths_257
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static IList<string> BinaryTreePaths(TreeNode root)
+        {
+
+        }
+    }
+}
