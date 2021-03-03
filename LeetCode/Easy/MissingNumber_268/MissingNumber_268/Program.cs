923a88b636fb4a45171a20b824061d324d9487d2
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-03-04 01:53:02 +0200

    no message

---
+++ LeetCode/Easy/MissingNumber_268/MissingNumber_268/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MissingNumber_268
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int MissingNumber(int[] nums)
+        {
+
+        }
+    }
+}
