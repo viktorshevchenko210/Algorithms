624c0d569c5cddfce4c62ed5c8e50b6cf4c1a33c
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-03-09 00:36:53 +0200

    no message

--- LeetCode/Easy/BinaryTreePaths_257/BinaryTreePaths_257/Program.cs
+++ LeetCode/Easy/BinaryTreePaths_257/BinaryTreePaths_257/Program.cs
@@ -7,12 +7,44 @@ namespace BinaryTreePaths_257
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.left.right = new TreeNode(5);
+
+            root.right = new TreeNode(3);
+
+            BinaryTreePaths(root);
         }
 
         static IList<string> BinaryTreePaths(TreeNode root)
         {
+            if (root == null)
+                return new List<string>();
 
+            return DFS(root, null, new List<string>());
+        }
+
+        static List<string> DFS(TreeNode node, string path, List<string> paths)
+        {
+            if (node == null)
+            {
+                return paths;
+            }
+
+            if (path != null)
+                path = $"{path}->{node.val}";
+            else
+                path = $"{node.val}";
+
+            if(node.left == null && node.right == null)
+            {
+                paths.Add(path);
+            }
+
+            DFS(node.left, path, paths);
+            DFS(node.right, path, paths);
+
+            return paths;
         }
     }
 }
