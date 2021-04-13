045ae7cd92113d6af4375b39f12e2ec04ea5c8b9
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-04-14 01:12:49 +0300

    no message

---
+++ LeetCode/FirstUniqueCharacterIsAString_387/FirstUniqueCharacterIsAString_387/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstUniqueCharacterIsAString_387
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstUniqChar("leetcode");
+        }
+
+        static int FirstUniqChar(string s)
+        {
+            Dictionary<char, Tuple<int,int>> map = new Dictionary<char, Tuple<int, int>>();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (map.ContainsKey(s[i]))
+                {
+                    var tuple = map[s[i]];
+                    map[s[i]] = Tuple.Create(tuple.Item1, tuple.Item2+1);
+                }
+                else
+                    map.Add(s[i], Tuple.Create(i, 1));
+            }
+
+            var item = map
+                .Where(s => s.Value.Item2 == 1)
+                .FirstOrDefault();
+
+            if (item.Value == null)
+                return -1;
+            else
+                return item.Value.Item1;
+        }
+    }
+}
---
+++ LeetCode/RansomNote_383/RansomNote_383/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RansomNote_383
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ransomNote = "aa";
+            string magazine = "aab";
+        }
+
+        static bool CanConstruct(string ransomNote, string magazine)
+        {
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                if (ransomNote[i] != magazine[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
